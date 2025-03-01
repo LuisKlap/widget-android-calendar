401f319e54c0b021741df4ae9026ab8941b091db
LuisKlap <luis_klap@outlook.com>
2025-03-01 20:14:39 -0300

    atualiza configurações do Gradle para SDK e NDK, adiciona suporte a Compose e dependências de widgets

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -2,12 +2,13 @@ plugins {
     id("com.android.application")
     id("kotlin-android")
     id("dev.flutter.flutter-gradle-plugin")
+    id("org.jetbrains.kotlin.android") version "1.8.22"
 }
 
 android {
     namespace = "com.example.flutter_application"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    compileSdk = 34 
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -20,10 +21,10 @@ android {
 
     defaultConfig {
         applicationId = "com.example.flutter_application"
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+        minSdk = 21 // Substitua pelo valor correto
+        targetSdk = 33 // Substitua pelo valor correto
+        versionCode = 1
+        versionName = "1.0"
     }
 
     buildTypes {
@@ -31,8 +32,22 @@ android {
             signingConfig = signingConfigs.getByName("debug")
         }
     }
+
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.4.0" // Atualize para uma versão compatível
+    }
 }
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    implementation("androidx.glance:glance-appwidget:1.0.0-alpha05")
+    implementation("androidx.compose.runtime:runtime:1.0.0")
+    implementation("androidx.compose.compiler:compiler:1.4.0") // Adicione esta linha
 }
